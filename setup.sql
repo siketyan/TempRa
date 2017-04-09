27f5ef24398d26fa54d85ca4b1238b61dde766d5
Siketyan <siketyan0130@gmail.com>
2017-04-09 13:22:27 +0900

    Add setup SQL file

---
+++ setup.sql
@@ -0,0 +1,45 @@
+CREATE TABLE `records` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `year` smallint(4) NOT NULL,
+  `month` tinyint(2) NOT NULL,
+  `day` tinyint(2) NOT NULL,
+  `hour` tinyint(2) NOT NULL,
+  `minute` tinyint(2) NOT NULL,
+  `temp` float NOT NULL,
+  `hum` float NOT NULL,
+  `pres` float NOT NULL,
+  PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8;
+
+CREATE TABLE `hours` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `year` smallint(4) NOT NULL,
+  `month` tinyint(2) NOT NULL,
+  `day` tinyint(2) NOT NULL,
+  `hour` tinyint(2) NOT NULL,
+  `temp` float NOT NULL,
+  `hum` float NOT NULL,
+  `pres` float NOT NULL,
+  PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8;
+
+CREATE TABLE `days` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `year` smallint(4) NOT NULL,
+  `month` tinyint(2) NOT NULL,
+  `day` tinyint(2) NOT NULL,
+  `temp` float NOT NULL,
+  `hum` float NOT NULL,
+  `pres` float NOT NULL,
+  PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8;
+
+CREATE TABLE `months` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `year` smallint(4) NOT NULL,
+  `month` tinyint(2) NOT NULL,
+  `temp` float NOT NULL,
+  `hum` float NOT NULL,
+  `pres` float NOT NULL,
+  PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8;
